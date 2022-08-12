79cc7a5b0afea7dc74168c93a50c0eb5263fc0c5
DirectDev <jcmassoco2@gmail.com>
2022-08-12 17:28:35 -0300

    Start Progamming

---
+++ assets/user.js
@@ -0,0 +1,8 @@
+const { REST } = require('@discordjs/rest');
+const { Client, Intents } = require('discord.js');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json')
+  
+  this.banner = data.banner;
+    } else if (this.banner !== https://i.imgur.com/paBWMDh.png) {
+      this.banner ??= undefined;
--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-
+import discord
 
 client = discord.client()
+
+def _prefix_callable(bot, msg):
+    user_id = bot.user.id
+    base = ['<@!{}> '.format(user_id), '<@{}> '.format(user_id)]
+    if msg.guild is None:
+        base.append('!')
+        base.append('?')
+    else:
+        base.extend(bot.prefixes.get(msg.guild.id, ['?', '!']))
+    return base
+
+@client.event
+async def on_message(message):
+
+        print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+  if message.author == client.user:
+    return
+        if message.content.startswith('prefix'):
+        response = SofiiaBot.request(message.content[4:])
+        await message.channel.send(response)
