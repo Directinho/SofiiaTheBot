aac28efdd82dc9231cf9d06c417bbd39d7b93759
Amongus <99887838+SussyBaka06@users.noreply.github.com>
2022-08-15 13:12:45 +0000

    SlashCommands Upgrade

---
+++ commands/slash.js
@@ -0,0 +1,37 @@
+const { REST } = require('@discordjs/rest');
+const { Client, Intents } = require('discord.js');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json')
+
+const clientId = '[IdKey]';
+const guildId = '[GuildKey]';
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
