9ef67623c16eb56eb967d8cfaca1642ca19b9b4b
DirectDev <69155843+Directinho@users.noreply.github.com>
2022-08-15 12:56:20 +0000

    User upgrade

---
+++ assets/rpc.js
@@ -0,0 +1,4 @@
+const { REST } = require('@discordjs/rest');
+const { Client, Intents } = require('discord.js');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json')
--- assets/user.js
+++ assets/user.js
@@ -2,4 +2,8 @@ const { REST } = require('@discordjs/rest');
 const { Client, Intents } = require('discord.js');
 const { Routes } = require('discord-api-types/v9');
 const { token } = require('./config.json')
+
+client.user.setBanner('https://ibb.co/wgRssvL')
+  .then(user => console.log(`New Banner set!`))
+  .catch(console.error)
   
